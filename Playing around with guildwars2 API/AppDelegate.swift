e23824da9b58fa18c1719570a15d3069f4271e00
jrtaylor95 <jrtaylor95@gmail.com>
2017-08-19 16:06:23 -0400

    Initial Commit

---
+++ Playing around with guildwars2 API/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Playing around with guildwars2 API
+//
+//  Created by James Taylor on 8/19/17.
+//  Copyright © 2017 James Taylor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Playing around with guildwars2 API/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Playing around with guildwars2 API
+//
+//  Created by James Taylor on 8/19/17.
+//  Copyright © 2017 James Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
